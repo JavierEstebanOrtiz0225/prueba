736909c04e65e792933e688ff3afe7df976a5a76
Javier Ortiz <jeortiz19@misena.edu.co>
2020-11-05 10:56:24 -0500

    prueba59

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 
 let server = require('./config/server');
-let shell = require('child_process').execSync
 
 server.listen(3001,()=>{
     console.log('Abierto en el puerto 3001');
-    shell('cd .. && cd component_test && node index.js')
+
 });
--- component_test/index.js
+++ component_test/index.js
@@ -1,10 +1,16 @@
 let axios = require('axios').default;
-axios.get(`http://localhost:3001/test/${process.env.DATA}`)
+let fs = require('fs')
+let response;
+axios.get(`https://youtube.com`)
 
 .then(data=>{
     console.log(data.data);
 })
 .catch(err=>{
-    console.log(err);
-});
+    if(typeof err.response == 'undefined'){
+        console.log(err);
+    }else{
+        console.log(err.response.data);
+    }
+})
 
