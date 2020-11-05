46a17e1fd7bcc752cc66042dee99893f74459649
Javier Ortiz <jeortiz19@misena.edu.co>
2020-11-05 08:45:42 -0500

    prueba45

--- component_test/index.js
+++ component_test/index.js
@@ -1,13 +1,14 @@
 let axios = require('axios').default;
 let fs = require('fs')
-let shell = require('child_process').execSync;
+let response;
 axios.get(`http://localhost:3001/test/${process.env.DATA}`)
 
 .then(data=>{
-    fs.appendFileSync('./artefact/response.html',data.data)
-    console.log(data.data);
+    print(data.data)
 })
 .catch(err=>{
-    fs.appendFileSync('./artefact/response.html',err.response.data)
-    console.log(err.response.data);
+    print(err.response.data)
 })
+
+console.log(data);
+let print = (data)=>{console.log(data);}
